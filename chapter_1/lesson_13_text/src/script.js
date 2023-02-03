672ba9f57c2c50cd6cc4909a18b3858177b2cf79
Aloys Berger <alo@reaktive.com>
2023-02-03 18:24:55 +1000

    feat: Finish lesson 13

---
+++ chapter_1/lesson_13_text/src/script.js
@@ -0,0 +1,142 @@
+import * as dat from 'lil-gui'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry"
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader"
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/9.png')
+const donutTexture = textureLoader.load('/textures/matcaps/5.png')
+
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader()
+fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
+    const textGeometry = new TextGeometry(
+        "Hello Alo!",
+        {
+            font,
+            size: 0.5,
+            height: 0.25,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 0.03,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 12
+        }
+    )
+    textGeometry.center();
+    const textMaterial = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
+    const text = new THREE.Mesh(textGeometry, textMaterial)
+    scene.add(text)
+
+    const donutGeometry = new THREE.TorusBufferGeometry(0.3,0.2,20,45);
+    const donutMaterial = new THREE.MeshMatcapMaterial({matcap: donutTexture})
+    for(let i = 0;i < 200; i++){
+        const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+
+        donut.position.x = (Math.random() - 0.5) * 10
+        donut.position.y = (Math.random() - 0.5) * 10
+        donut.position.z = (Math.random() - 0.5) * 10
+
+        donut.rotation.x = Math.random() * Math.PI
+        donut.rotation.y = Math.random() * Math.PI
+
+        const scale = Math.random();
+        donut.scale.x = scale
+        donut.scale.y = scale
+        donut.scale.z = scale
+
+
+
+        scene.add(donut);
+    }
+})
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
